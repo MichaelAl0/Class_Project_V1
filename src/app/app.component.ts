b9ddb8716c6f7a96543a93268231c51bd58e0205
Michael Harris <alphasni@mail.com>
2022-04-07 20:03:34 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Class_Project_V1';
+  title = 'AnyCite';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CitationComponent } from './citation/citation.component';
+import { BookComponent } from './dashboard/book/book.component';
+import { JournalComponent } from './dashboard/journal/journal.component';
+import { WebsiteComponent } from './dashboard/website/website.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    CitationComponent
+    CitationComponent,
+    BookComponent,
+    JournalComponent,
+    WebsiteComponent
   ],
   imports: [
     BrowserModule
